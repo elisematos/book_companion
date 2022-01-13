3667ea120a114363d156de952bd8238cc8c08719
Elise MATOS <mme.matos.elise@gmail.com>
2022-01-13 10:57:16 +0100

    initial commit : SearchController

---
+++ src/Service/BooksApiService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Dotenv\Dotenv;
+
+class BooksApiService
+{
+    private $client;
+    const API_KEY = "AIzaSyAF_ZiJtyS6sjfNaW12xx7a8yJOeHzFJQ8";
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getBooks(): array {
+        $response = $this->client->request(
+            'GET',
+            'https://www.googleapis.com/books/v1/volumes?', [
+                'query' => [
+                    'q' => 'mousquetaire',
+                    'key' => BooksApiService::API_KEY
+                ]
+            ]
+        );
+        return $response->toArray();
+    }
+}
